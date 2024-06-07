58843b09f02600ab11ded4a64d777a94e6b2ec5a
AndrewIndeche <indecheandrew@outlook.com>
2024-06-07 16:24:27 +0300

    add search button

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment, TextField } from '@mui/material';
 
 const Searchbar = () => (
   <div className="searchBarContainer">
-    <input className="searchBar" type="text" />
+    <TextField
+      className="searchBar"
+      placeholder="Enter your search query"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+    <button type="button">Search</button>
   </div>
 );
 
