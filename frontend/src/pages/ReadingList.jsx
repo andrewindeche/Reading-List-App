6adc62739260cbc9032b1ac7896771cdd90df335
AndrewIndeche <indecheandrew@outlook.com>
2024-06-07 23:11:32 +0300

    add sample profile image

--- frontend/src/pages/ReadingList.jsx
+++ frontend/src/pages/ReadingList.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 const Readinglist = () => (
-  <div>
-    <p>Return To Search</p>
+  <div className="Readinglist">
+    <img
+      src="https://www.shutterstock.com/image-vector/young-cute-monkey-baby-sweet-600nw-2270033121.jpg"
+      alt="Profile"
+      className="profileImage"
+    />
   </div>
 );
 export default Readinglist;
