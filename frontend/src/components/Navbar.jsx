24116064128bdd9809b63758c83460de6e9da20e
AndrewIndeche <indecheandrew@outlook.com>
2024-06-07 14:27:58 +0300

    style nav links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Navbar = () => (
-  <img className="navImage" src="https://ml.globenewswire.com/Resource/Download/671a4959-db29-4139-a53e-5ca7e3294702?size=2" alt="Ello Logo" />
+  <div className="navBar">
+    <img className="navImage" src="https://ml.globenewswire.com/Resource/Download/671a4959-db29-4139-a53e-5ca7e3294702?size=2" alt="Ello Logo" />
+    <span className="navLinks">
+      <p>Search</p>
+      <p>My Reading List</p>
+    </span>
+  </div>
 );
 export default Navbar;
